177676119fa567e3e00fa65ac5d4c90f22152b46
A252433 <A252433@LSBSA-10L6HW3>
2023-12-05 09:09:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l = 5;
+
+            for (int i = 0; i < l; i++) {
+            
+                for (int j = 0; j < l; j++)
+                {
+
+                    if ((j == l-1-i) )//&& (i != l - 1))
+                    { 
+                        Console.Write("/");
+                    }
+                    else if (i == l-1)
+                    {
+                        Console.Write("_");   
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("|");
+            }
+        }
+    }
+}
